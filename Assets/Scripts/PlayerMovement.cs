cdd841136499cc695be9098cb55a4e8ce76d0ae4
Deacon <deaconmizzi@gmail.com>
2025-01-20 10:39:44 +0100

    Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movement Parameters")]
+    public float moveSpeed = 5f; // Speed for left/right movement
+    public float jumpForce = 7f; // Force applied for jumping
+    public int maxJumps = 2;     // Maximum number of jumps allowed
+
+    private Rigidbody2D rb;
+    private int jumpCount;       // Tracks the current number of jumps
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // Reference Rigidbody2D
+        jumpCount = maxJumps;             // Initialize jump count
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Horizontal Movement
+        float moveInput = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow Keys
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--; // Decrease jump count
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Reset jump count when touching any collider
+        jumpCount = maxJumps;
     }
 }
